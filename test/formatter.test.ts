13e4114fefde67acbf4aeeae6ab8b586f64b6a85
masaminh <33191610+masaminh@users.noreply.github.com>
2021-05-09 11:31:55 +0900

    当日判定の誤りを修正 (#1)
    * フォーマット文字列の誤りを修正
    * UTCで判定していたのを修正

---
+++ test/formatter.test.ts
@@ -0,0 +1,29 @@
+import * as luxon from 'luxon';
+import * as formatter from '../lib/formatter';
+
+describe('formatter', (): void => {
+  type TestArgs= {
+    eventTime: string;
+    expectedMessage: string;
+  };
+
+  beforeEach(() => {
+    luxon.Settings.defaultZoneName = 'utc';
+  });
+
+  test.each`
+    eventTime | expectedMessage
+    ${'2021-05-01T00:00:00Z'} | ${'今日は20:00から21:00にTITLEがあります。'}
+    ${'2021-04-30T23:59:59Z'} | ${'今日は20:00から21:00にTITLEがあります。'}
+    ${'2021-04-30T15:00:00Z'} | ${'今日は20:00から21:00にTITLEがあります。'}
+    ${'2021-04-30T14:59:59Z'} | ${'今日はイッテQはありませんが、5月1日20:00から21:00にあるようです。'}
+  `(
+    '5/1に番組がある場合、$eventTime の時のメッセージは「 $expectedMessage 」である',
+    ({ eventTime, expectedMessage }: TestArgs) => {
+      const actualMessage = formatter.format([{
+        date: '5/1', fromtime: '20:00', totime: '21:00', title: 'TITLE',
+      }], eventTime);
+      expect(actualMessage).toBe(expectedMessage);
+    },
+  )
+})
